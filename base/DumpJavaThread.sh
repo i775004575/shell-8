fb83dc7e06cc927fc83c5cfec94aca85fd5d8a4f
= <=>
2013-03-31 18:02:24 +0800

    update

--- base/DumpJavaThread.sh
+++ base/DumpJavaThread.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
-
 export LC_ALL=C
 
-level="10"
+##params
+level=$1
+if [ -z "$level" ] ;then
+level=10
+fi
+trytimes="1000"
+
+##prepare 4 dump
 path="/tmp/dump_log_jstack/"
 rm -rf $path
 mkdir $path
 file="d_"
 pid=`ps -ef | fgrep jboss  | fgrep java | awk '{print $2}'`
 
-for i in `seq 100`
+
+for i in `seq $trytimes`
 do
 time=`date +%s`"_"`date +%N`
 rtop=`top -p $pid  -H -d 1 -n 1 | head -8 | tail -1`
-ppid=`echo $rtop | awk '{print $1}'`
+ppid=`echo $rtop | awk '{print $1}' | awk '{print substr($0,7)}'`
 cpu=`echo $rtop | awk '{print $9}' | awk -F"." '{print $1}'`
 istrue=`expr $cpu \> $level`
 if [ $istrue = "1"  ] ;then
-echo $rtop >> $path$file$time
-echo $ppid | awk '{print substr($0,7)}' |awk '{printf "%x\n",$0}' >>$path$file$time
 /opt/taobao/java/bin/jstack `ps -ef | fgrep jboss  | fgrep java | awk '{print $2}'`   >> $path$file$time
+echo $rtop >> $path$file$time
+echo $ppid |awk '{printf "%x\n",$0}' >>$path$file$time
+echo "Bingo Bingo"
 fi
+remain=`expr $trytimes - $i`
+echo $remain
 done
