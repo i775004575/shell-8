09424dea9c4356562c83eccb345b5acc58e902cb
= <=>
2013-05-10 23:03:06 +0800

    update

--- base/SplitApacheLog.sh
+++ base/SplitApacheLog.sh
@@ -2,43 +2,59 @@
 export LC_ALL=C
 
 ###
-###  items:  ip cost time method url status size ref ua  uri  refuri
+###  items:  ip cost time method url status size ref ua uri refuri date all
 ###
 ###  sh sp.sh -def ip,time,ref 
-###
-###  cat access.log | sh sp.sh -pipe  ip,time 
+###  sh sp.sh -deftail ip,time,ref 
+###  sh sp.sh /home/admin/logs/access.log ip,time,ref 
+###  cat access.log | sh sp.sh -pipe ip,time 
 ###
 
-
+#####################CORE################
 handle () {
 items=$1
 sep=$2
+file=$3
+
 awk -v sep="$sep" "
-BEGIN{ORS=sep}
+BEGIN{
+	ORS=sep
+}
 {
 	date=substr(\$4,2,11);
 	\$4=substr(\$4,14);
 	\$6=substr(\$6,2);
 	\$7=substr(\$7,0,length(\$7)-1);
-	\$10=substr(\$10,2,length(\$10)-2);
-	for(i=11;i<=NF;i++){r=r\$i\" \";}
-	\$11=substr(r,2,length(r)-3);
-	r=\"\";
 	uri=tolower(substr(\$7,0,index(\$7,\"?\")-1)); 
 	if(uri==\"\"){uri=\$7;}
+	\$10=substr(\$10,2,length(\$10)-2);
 	refuri=tolower(substr(\$10,0,index(\$10,\"?\")-1)); 
 	if(refuri==\"\"){refuri=\$10;}
+	tmpr=\"\";
+	for(i=11;i<=NF;i++){tmpr=tmpr\$i\" \";}
+	\$11=substr(tmpr,2,length(tmpr)-3);
 	print $items
-}"
+}"  $file
 }
 
-path="/home/admin/cai/logs/cronolog/"
+########START########
+
+path="/home/admin/cai/logs/cronolog"
 year=`date +%Y`
 month=`date +%m`
 today=`date +%Y-%m-%d`
+defaultlog=$path"/"$year"/"$month"/"$today"-taobao-access_log"
 
 log=$1
 items=$2
+sep=$3
+if [ -z "$2" ];then
+items="all"
+fi
+if [ -z "$3" ];then
+sep="\n"
+fi
+
 items=`echo $items|awk -F"," '
 BEGIN{
 	map["all"]="$0";
@@ -61,22 +77,16 @@ BEGIN{
 		if(i<NF){res=res",";}
 	}
 }
-END{print res;}'`
+END{
+	print res;
+}'`
 
-if [ -z "$3" ];then
-sep="\n"
-else
-sep=$3
-fi
-
-if [ $1 = "-pipe" ];then
+if [ $log = "-pipe" ];then
 handle $items "$sep"
-elif [ $1 = "-deftail" ];then
-log=$path$year"/"$month"/"$today"-taobao-access_log"
-tail -10f $log | handle $items "$sep"
-elif [ $1 = "-def" ];then
-log=$path$year"/"$month"/"$today"-taobao-access_log"
-cat $log | handle  $items "$sep"
+elif [ $log = "-deftail" ];then
+tail -f $defaultlog | handle $items "$sep"
+elif [ $log = "-def" ];then
+handle  $items "$sep" $defaultlog
 else
-cat $log | handle  $items "$sep"
+handle  $items "$sep" $log
 fi
