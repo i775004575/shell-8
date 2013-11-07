1338cee7e9f850b874319e2f43d2577f26996da1
peiliping <peilipingplp@gmail.com>
2013-11-07 16:56:12 +0800

    update

---
+++ base/SVNCommit.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+clear
+svnURL=`svn info | fgrep URL | awk -F"URL:" '{ print $2}'`
+appname=`svn info | fgrep 版本库根 | awk -F"/" '{print $NF}' `
+svntrunk="http://svn.app.taobao.net/repos/"$appname"/trunk/"$appname
+
+echo  "AppName:" $appname
+
+if [ $svnURL = $svntrunk ] ;then
+echo  "==============================="
+echo  ""
+echo  "WARN: It is trunk !!! Really???"
+echo  ""
+fi
+
+tmpresult="/tmp/svntempresult"
+svn st > $tmpresult
+
+t1=`awk '{if($1=="M") print $0}' $tmpresult`
+if [ "$t1" != "" ] ;then
+echo  "==============================="
+echo  "These files modified will be commit!!!"
+echo  ""
+awk '{if($1=="M") print $0}' $tmpresult
+fi
+
+t1=`awk '{if($1=="A") print $0}' $tmpresult`
+if [ "$t1" != "" ] ;then
+echo  "==============================="
+echo  "These files added will be commit"
+echo  ""
+awk '{if($1=="A") print $0}' $tmpresult
+fi
+
+t1=`awk '{if($1=="D") print $0}' $tmpresult`
+if [ "$t1" != "" ] ;then
+echo  "==============================="
+echo  "These files deleted will be commit"
+echo  ""
+awk '{if($1=="D") print $0}' $tmpresult
+fi
+
+t1=`awk '{if($1=="?") print $0}' $tmpresult`
+if [ "$t1" != "" ] ;then
+echo  "==============================="
+echo  "These files not in svn will not be commit"
+echo  ""
+awk '{if($1=="?") print $0}' $tmpresult
+fi
+
+echo  "==============================="
+echo  "Will You Commit? (y/n)"
+read  ans
+case $ans in 
+y|Y|yes|YES)
+ echo "Enter Reason For Commit!"
+ read reason
+ echo "Who Review This Commit"
+ read review
+ message="Reason: $reason    Reviewer: $review"
+ svn ci -m "$message"
+ svn up 
+;;
+
+n|N|no|NO)
+ exit 0
+;;
+esac
