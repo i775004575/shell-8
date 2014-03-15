77766e8a66dcef081dab34a5b1a197fe8580a918
peiliping <peilipingplp@gmail.com>
2014-03-15 22:59:56 +0800

    update

--- lua/ds.lua
+++
@@ -1,8 +0,0 @@
-local name = ngx.var.arg_dynamicdatasourcename
-local token = ngx.var.arg_token
-local ip = ngx.var.arg_ip
-local f = io.popen("awk -F '{if($1==\""..ip.."\"&&$2==\""..token.."\"&&$3==\""..name.."\"){print $4}}' /home/peiliping/dev/linkall/tengine/conf/lua/dsfile")
-for line in f:lines() do 
-  ngx.say(line)
-end 
-f:close()
