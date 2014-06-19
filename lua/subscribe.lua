5416a1aa7b54f57031febf4b48e5bfdeaf8a013c
peiliping <peilipingplp@gmail.com>
2014-06-19 16:43:04 +0800

    update

--- lua/subscribe.lua
+++
@@ -1,8 +0,0 @@
-local id = ngx.var.arg_topicId
-local action = ngx.var.arg_action
-local ip = ngx.var.arg_ip
-local f = io.popen("awk -F '{if($1==\""..id.."\"&&$2==\""..action.."\"&&$3==\""..ip.."\"){print $4}}' /home/peiliping/dev/linkall/tengine/conf/lua/datafile")
-for line in f:lines() do 
-  ngx.say(line)
-end 
-f:close()
