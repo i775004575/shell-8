b48722dcb80011a17427815a6a11dba21b0407e9
peiliping <peilipingplp@gmail.com>
2014-07-08 11:54:36 +0800

    update

--- base/fanqiang.sh
+++ base/fanqiang.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect  
 set timeout 100  
-spawn ssh -N -v useruseruser@xxx.xxx.xxx.xxx -D 127.0.0.1:7070
+spawn ssh -N -p xxx -v root@xxx.xxx.xxx.xxx -D 127.0.0.1:7070
 expect "password:"  
-send "mimamimamimamima\r"  
+send "mimimimimimimimimimi\r"  
 interact
--- base/tools.sh
+++ base/tools.sh
@@ -15,18 +15,13 @@ echo "\t1  \t翻墙"
 echo "\t2  \t修改hosts文件"
 echo "\t3  \t本机IP\n"
 echo "\t4  \t启动Tomcat服务器"
-echo "\t5  \t启动带Agent的Tomcat服务器"
-echo "\t6  \t关闭Tomcat服务器\n"
-echo "\t7  \t启动Tengine服务"
-echo "\t8  \t关闭Tengine服务"
-echo "\t9  \t重启Tengine服务\n"
-echo "\t10 \t启动mysql服务"
-echo "\t11 \t关闭mysql服务\n"
-echo "\t12 \t启动FastCGI\n"
-echo "\t13 \tUI界面登陆oracle\n"
-echo "\t14 \tMaven环境切换到公司"
-echo "\t15 \tMaven环境切换到公网\n"
-echo "\t16 \t测试peiliping-agent\n"
+echo "\t5  \t关闭Tomcat服务器\n"
+echo "\t6  \t启动Tengine服务"
+echo "\t7  \t关闭Tengine服务"
+echo "\t8  \t重启Tengine服务\n"
+echo "\t9 \t启动mysql服务"
+echo "\t10 \t关闭mysql服务\n"
+echo "\t11 \t启动FastCGI\n"
 echo "\t99 \t寻找快捷方式\n"
 echo "\t0  \texit\n"
 echo "which One ?"
@@ -51,47 +46,29 @@ sh $TOMCATZSHPATH deploy
 getstat java
 ;;
 5)
-sh $TOMCATZSHPATH deploywithagent
-getstat java
-;;
-6)
-##sh $TOMCATZSHPATH shutdown
 kill -9 `pgrep java`
 getstat java
 ;;
-7)
+6)
 sh $NGINXYSHPATH start
 getstat nginx 
 ;;
-8)
+7)
 sh $NGINXYSHPATH stop
 getstat nginx 
 ;;
-9)
+8)
 sh $NGINXYSHPATH restart
 getstat nginx 
 ;;
-10)
+9)
 /etc/init.d/mysql start
 ;;
-11)
+10)
 sudo /etc/init.d/mysql stop
 ;;
-12)
+11)
 spawn-fcgi -a 127.0.0.1 -p 9000 -C 10 -u peiliping -f /usr/bin/php-cgi
-;;
-13)
-sqldeveloper
-;;
-14)
-sudo cp /etc/maven2/settings.xmlblue /etc/maven2/settings.xml
-;;
-15)
-sudo cp /etc/maven2/settings.xmldef /etc/maven2/settings.xml
-;;
-16)
-sh $TOMCATZSHPATH deploywithpeilipingagent
-getstat java
 ;;
 99)
 nautilus /usr/share/applications/
