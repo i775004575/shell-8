ed0ed768ce31d5c70b92156d85b85e649c299748
peiliping <peilipingplp@gmail.com>
2015-05-19 14:39:48 +0800

    update

--- base/tools.sh
+++ base/tools.sh
@@ -11,15 +11,10 @@ ps -ef | fgrep $1
 if [ -z "$1" ] ;then
 clear
 echo "=============================================="
-echo "\t1  \t"
+echo "\n"
+echo "\t1  \t本机IP"
 echo "\t2  \t修改hosts文件"
-echo "\t3  \t本机IP\n"
-echo "\t4  \t启动Tomcat服务器"
-echo "\t5  \t关闭Tomcat服务器\n"
-echo "\t6  \t启动Tengine服务"
-echo "\t7  \t关闭Tengine服务"
-echo "\t8  \t重启Tengine服务\n"
-echo "\t99 \t寻找快捷方式\n"
+echo "\t3  \t寻找快捷方式\n"
 echo "\t0  \texit\n"
 echo "which One ?"
 echo "=============================================="
@@ -30,34 +25,12 @@ fi
 
 case $ans in
 1)
+ifconfig
 ;;
 2)
 sudo gedit /etc/hosts
 ;;
 3)
-ifconfig
-;;
-4)
-sh $TOMCATZSHPATH deploy
-getstat tomcat
-;;
-5)
-kill -9 `pgrep tomcat`
-getstat tomcat
-;;
-6)
-sh $NGINXYSHPATH start
-getstat nginx 
-;;
-7)
-sh $NGINXYSHPATH stop
-getstat nginx 
-;;
-8)
-sh $NGINXYSHPATH restart
-getstat nginx 
-;;
-99)
 nautilus /usr/share/applications/
 ;;
 0)
